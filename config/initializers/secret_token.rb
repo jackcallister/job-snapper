307ba8f6ab0a5234c2f5cba9eddffe41f174faee
Jack Callister <jarsbe@gmail.com>
2013-04-23 21:29:22 +1200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JobSnapper::Application.config.secret_key_base = '4abf421df29eae228f80c722b365b317a8c3f530caa246febca30a232a6b820cd92e9473c2c9b8781745380cee8686f0c2a38a9186e239398613b3e8b20c0066'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JobSnapper::Application.config.session_store :encrypted_cookie_store, key: '_job-snapper_session'
