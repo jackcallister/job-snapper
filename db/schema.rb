fac396fbe81c6f8f0483ed2e62e05d46c91b7c77
Jack Callister <jarsbe@gmail.com>
2013-04-26 08:32:06 +1200

    Added candidate and split up views.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130425054214) do
+ActiveRecord::Schema.define(version: 20130425200831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "candidates", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "candidates", ["email"], name: "index_candidates_on_email", unique: true
+  add_index "candidates", ["reset_password_token"], name: "index_candidates_on_reset_password_token", unique: true
 
   create_table "employers", force: true do |t|
     t.string   "email",                  default: "", null: false
