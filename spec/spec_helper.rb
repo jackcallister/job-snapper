752c3682597938259bad729199e1f82ec2f4a73b
Jack Callister <jarsbe@gmail.com>
2013-04-25 18:36:37 +1200

    Added employer devise resource.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Include Devise test helpers.
+  config.include Devise::TestHelpers, :type => :controller
 end
