c8a9596c2dd2fe3ff19b8d66d50083d3f51f58ed
Jack Callister <jarsbe@gmail.com>
2013-04-26 09:58:55 +1200

    Added HTTP auth.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate! unless Rails.env == "development"
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def authenticate!
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["HTTP_AUTH_USERNAME"] && password == ENV["HTTP_AUTH_PASSWORD"]
+    end
+  end
 end
