ced7af610412b2ce7ad0d8b9c9fe6d2710bf88f7
Jack Callister <jarsbe@gmail.com>
2013-04-29 14:54:45 +1200

    Specs passing. Added outline feature spec files.

--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Candidate do
   it { should have_one(:profile) }
+  it { should have_many(:applications) }
 end
--- spec/models/employer_spec.rb
+++ spec/models/employer_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Employer do
   it { should have_one(:profile) }
+  it { should have_many(:jobs) }
 end
