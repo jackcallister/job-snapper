e8b6959e2488256bb6ee1e101021445d8fe8493b
Jack Callister <jarsbe@gmail.com>
2013-05-18 09:07:22 +1200

    Added grav to application show.

--- app/controllers/jobs/applications_controller.rb
+++ app/controllers/jobs/applications_controller.rb
@@ -8,6 +8,7 @@ class Jobs::ApplicationsController < ActionController::Base
   def show
     @job = Job.find(params[:job_id])
     @application = Application.find(params[:id])
+    @candidate = Candidate.find(@application.candidate_id)
     @candidate_profile = Candidate::Profile.find(@application.candidate_id)
     render :layout => "application"
   end
