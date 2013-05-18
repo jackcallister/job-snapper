53f8e6fe871858d6d0d82b5fa8b28bddcf05bfb1
Jack Callister <jarsbe@gmail.com>
2013-05-18 12:05:42 +1200

    Removed reg controller.

--- app/controllers/registrations_controller.rb
+++
@@ -1,7 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-  after_filter :initialize_profile, :only => [:create]
-
-  def initialize_profile
-    self.resource.build_profile.save
-  end
-end
