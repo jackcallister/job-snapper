97a65eec53a49ba5dc93c6cc6bd6877a0b4362dc
Josh McArthur <joshua.mcarthur@gmail.com>
2013-05-20 09:13:06 +1200

    When an employer is destroyed, also destroy their jobs

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,7 +1,7 @@
 class Employer < User
   has_one :user, as: :profile, dependent: :destroy
   has_one :profile, :class_name => "Employer::Profile"
-  has_many :jobs
+  has_many :jobs, :dependent => :destroy
 
   def can_apply_to(job)
     false
