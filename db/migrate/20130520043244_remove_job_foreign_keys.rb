6719987cbe34dcf26db3baaabbfb6bd002f615bf
Josh McArthur <joshua.mcarthur@gmail.com>
2013-05-20 20:18:37 +1200

    Remove unnecessary foreign keys from jobs table

--- db/migrate/20130520043244_remove_job_foreign_keys.rb
+++ db/migrate/20130520043244_remove_job_foreign_keys.rb
@@ -1,5 +1,11 @@
 class RemoveJobForeignKeys < ActiveRecord::Migration
-  def add
-    remove
+  def up
+    remove_column :jobs, :type_id
+    remove_column :jobs, :category_id
+  end
+
+  def down
+    add_column :jobs, :type_id, :integer
+    add_column :jobs, :category_id, :integer
   end
 end
