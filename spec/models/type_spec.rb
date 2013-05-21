c09b614ead5d097ce50bc0efa03ae57b4f108360
Josh McArthur <joshua.mcarthur@gmail.com>
2013-05-21 23:45:38 +1200

    Spec type model

--- spec/models/type_spec.rb
+++ spec/models/type_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Type do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many :classifications }
+  it { should have_many(:jobs).through(:classifications) }
+  it { should validate_presence_of(:title) }
 end
